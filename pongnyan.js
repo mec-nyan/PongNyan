bffc783180e54abe39c27b2fdf59c95b9d463783
mec-nyan <marianoraspberrypi@gmail.com>
2021-11-09 23:14:39 -0300

    new style, dinamic position and size

--- pongnyan.js
+++ pongnyan.js
@@ -8,12 +8,31 @@ root.appendChild(field);
 const pad = document.createElement('div');
 pad.className = 'pad';
 field.appendChild(pad);
-/*
-const pad = document.getElementById('padRight');
+
+const fieldWidth = 600;
+const fieldHeight = 450;
+const padWidth = 100;
+const padHeight = 20;
+const padBotPos = 40;
+
+field.style.width = fieldWidth + 'px';
+field.style.height = fieldHeight + 'px';
+
+pad.style.width = padWidth + 'px';
+pad.style.height = padHeight + 'px';
+pad.style.bottom = padBotPos + 'px';
+pad.style.left = (fieldWidth - padWidth) / 2 + 'px';
+
+const fieldRect = field.getBoundingClientRect();
+const minLeftPos = fieldRect.left + 10;
+const maxRightPos = fieldRect.right - 10;
 
 document.body.onmousemove = (e) => {
-  if (e.clientY >= 50 && e.clientY <= 400) {
-    pad.style.top = `${e.clientY}px`;
+  if (e.clientX >= minLeftPos && e.clientX + padWidth <= maxRightPos) {
+    pad.style.left = e.clientX - fieldRect.left + 'px';
+  } else if (e.clientX <= minLeftPos) {
+    pad.style.left = minLeftPos - fieldRect.left + 'px';
+  } else if (e.clientX + padWidth >= maxRightPos) {
+    pad.style.left = maxRightPos - fieldRect.left - padWidth + 'px';
   }
 }
-*/
